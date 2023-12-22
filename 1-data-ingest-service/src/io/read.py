b91c1e061fee11f1663f5f78ac4e0d74a5bb6104
Collin Guidry <48544496+jcguidry@users.noreply.github.com>
2023-12-22 20:35:53 +0000

    added airport-level query

--- 1-data-ingest-service/src/io/read.py
+++ 1-data-ingest-service/src/io/read.py
@@ -23,3 +23,28 @@ def get_flight_data(api, identifier, start_datetime, end_datetime, max_pages=40)
     df = pd.json_normalize(data, 'flights')
 
     return df
+
+
+def get_airport_departures(api, airport_id, start_datetime, end_datetime, max_pages='5', cursor='1'):
+    """
+    # Get 
+    """
+    
+    endpoint = f'/airports/{airport_id}/flights/departures'
+    airline = 'AAL'
+    try:
+        data = api.query(endpoint, 
+                airline = airline,
+                end= end_datetime, 
+                start= start_datetime, 
+                max_pages=max_pages,
+                cursor=cursor)
+
+    except requests.HTTPError as e:
+        print(e)
+    except Exception as e:
+        logging.error(f"An unexpected Flight API error occurred: {e}")
+
+    return pd.json_normalize(data, 'departures')
+
+
